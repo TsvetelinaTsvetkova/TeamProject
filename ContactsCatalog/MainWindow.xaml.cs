100b48c76f311a9dc8143c534417530bb5a687fd
TsvetelinaTsvetkova <cvetelina.cvetkova@yahoo.com>
2017-04-18 16:09:02 +0300

    added contacts

--- ContactsCatalog/MainWindow.xaml.cs
+++ ContactsCatalog/MainWindow.xaml.cs
@@ -22,39 +22,72 @@ namespace ContactsCatalog
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+        private ContactsCatalogContext context;
+
         public MainWindow()
         {
             InitializeComponent();
 
             var context = new ContactsCatalogContext();
 
-            var first = new Contact
-            {
-                Name = "Ivan",
-                Address = "Vasil Levski 5",
-                ContactInformation = "School"
-            };
+            //var first = new Contact
+            //{
+            //    Name = "Ivan",
+            //    Address = "Vasil Levski 5",
+            //    ContactInformation = "School"
+            //};
 
-            var second = new Contact
-            {
-                Name = "Petur",
-                Address = "Vasil Levski 10",
-                ContactInformation = "Work"
-            };
+            //var second = new Contact
+            //{
+            //    Name = "Petur",
+            //    Address = "Vasil Levski 10",
+            //    ContactInformation = "Work"
+            //};
 
-            var third = new Contact
-            {
-                Name = "Milena",
-                Address = "Ivan Vazov 10",
-                ContactInformation = "Family"
-            };
+            //var third = new Contact
+            //{
+            //    Name = "Milena",
+            //    Address = "Ivan Vazov 10",
+            //    ContactInformation = "Family"
+            //};
 
-            context.Contacts.Add(first);
-            context.Contacts.Add(second);
-            context.Contacts.Add(third);
+            //context.Contacts.Add(first);
+            //context.Contacts.Add(second);
+            //context.Contacts.Add(third);
+            //context.SaveChanges();
+
+            DataContext = GetAllContacts(context);
+        }
+
+        private List<Contact> GetAllContacts(ContactsCatalogContext context)
+        {
+            return context.Contacts.ToList();
+        }
+
+        private void SaveChangesClick_Click(object sender,
+           RoutedEventArgs e)
+        {
             context.SaveChanges();
-            context.Contacts.Count();
+        }
+
+        public void LoadContacts()
+        {
+            DataContext = GetAllContacts(context);
+        }
+
+        private void AddNewContact_Click(object sender,
+           RoutedEventArgs e)
+        {
+            ((List<Contact>)DataContext).Add(AddNewContact());
+        }
+
+        private Contact AddNewContact()
+        {
+            var newContact = new Contact
+            {
+                Name = "'New Contact"
+            };
+            return (newContact);
         }
     }
 }
