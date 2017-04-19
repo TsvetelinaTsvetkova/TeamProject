d787664ffcb77c3f51ce74e32040ea488cd529ca
TsvetelinaTsvetkova <cvetelina.cvetkova@yahoo.com>
2017-04-19 13:49:24 +0300

    seed info

--- ContactsCatalog.Data/ContactsCatalogDbInitializer.cs
+++ ContactsCatalog.Data/ContactsCatalogDbInitializer.cs
@@ -16,8 +16,11 @@ namespace ContactsCatalog.Data
             var first = new Contact
             {
                 Name = "Ivan",
-                Address = "Iv",
-                ContactInformation = "Ivv"
+                Address = "Vasil Levski 10",
+                ContactInformation = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                                     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                                     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                                     "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
             };
             context.Contacts.Add(first);
             context.SaveChanges();
